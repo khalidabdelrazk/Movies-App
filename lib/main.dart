12c2785d7d2411e5bc40521191d92c540444aea8
khalid <khalidabdelrazk4@gmail.com>
2025-06-04 11:57:00 +0300

    changed application icon , name and added Easy Localization support en , ar

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:movies/core/routes/route_names.dart';
 import 'package:movies/core/routes/routes.dart';
 import 'package:movies/core/theme/app_theme.dart';
+import 'package:movies/generated/codegen_loader.g.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ar')],
+      path:
+          'assets/translations', // <-- change the path of the translation files
+      fallbackLocale: Locale('en'),
+      assetLoader: CodegenLoader(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       routes: Routes.myAppRoutes,
--- lib/presentation/home/ui/home_page.dart
+++ lib/presentation/home/ui/home_page.dart
@@ -1,14 +1,40 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:movies/generated/locale_keys.g.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.red,
       width: double.infinity,
       height: double.infinity,
+      child: Column(
+        children: [
+          SizedBox(height: 100,),
+          Text(LocaleKeys.profile_settings_avatar_label.tr()),
+          Text(LocaleKeys.onboarding_explore_button.tr()),
+          Text(LocaleKeys.authentication_confirm_password_label.tr()),
+          ElevatedButton(
+            onPressed: () {
+              if (context.locale == Locale('en')) {
+                context.setLocale(Locale('ar'));
+              } else {
+                context.setLocale(Locale('en'));
+              }
+              setState(() {});
+            },
+            child: Text("press"),
+          ),
+        ],
+      ),
     );
   }
 }
