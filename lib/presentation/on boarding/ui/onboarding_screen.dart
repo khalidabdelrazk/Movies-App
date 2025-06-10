043c845f4e5e2c61e9d8410747851e1216468d22
mahmoud <m.osama2275@nu>
2025-06-10 18:27:39 +0300

    Splash screen and Onboarding screens

--- lib/presentation/on boarding/ui/onboarding_screen.dart
+++ lib/presentation/on boarding/ui/onboarding_screen.dart
@@ -1,10 +1,217 @@
-import 'package:flutter/cupertino.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:movies/core/assets/app_assets.dart';
+import 'package:movies/core/routes/route_names.dart';
+import 'package:movies/core/theme/app_colors.dart';
+import 'package:movies/core/theme/app_styles.dart';
+import 'package:movies/core/utils/custom_button.dart';
+import 'package:movies/generated/locale_keys.g.dart';
+import 'package:movies/main.dart';
 
-class Onboarding extends StatelessWidget {
-  const Onboarding({super.key});
+class OnboardingScreen extends StatefulWidget {
+  
 
+  const OnboardingScreen({super.key});
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+final introKey = GlobalKey<IntroductionScreenState>();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    
+    return IntroductionScreen(
+      showBottomPart: false,
+      key: introKey ,
+      pages: [
+        PageViewModel(
+          titleWidget: Container(
+
+          ),
+          backgroundImage: AppAssets.onBoarding1,
+          bodyWidget: Container(
+      
+              margin: EdgeInsets.only(top: height * 0.10),
+            child: Column(
+              children: [
+                 Text(LocaleKeys.onboarding_onboarding_1_title.tr(),style: AppStyles.lightMedium36,
+                  textAlign: TextAlign.center,),
+                  SizedBox(height: height * 0.02,),
+                  Text(LocaleKeys.onboarding_onboarding_1_description.tr(),
+                  style: AppStyles.darkGrayRegular20,),
+                  SizedBox(height: height * 0.03,),
+                  
+                  CustomButton(body: Text(LocaleKeys.onboarding_explore_button.tr(),style: AppStyles.darkSemiBold20,),
+                  backgroundColor: null ,color: AppColors.primaryYellowColor,onPressed: (){
+                    introKey.currentState?.next();
+                  }),
+
+              ],
+            ),
+          )
+        ),
+
+       PageViewModel(
+          titleWidget: Container(
+          ),
+          backgroundImage: AppAssets.onBoarding2,
+          bodyWidget: Container(
+              margin: EdgeInsets.only(top: height * 0.13),
+            child: Column(
+              children: [
+                 Text(LocaleKeys.onboarding_onboarding_2_title.tr(),style: AppStyles.lightMedium36,
+                  textAlign: TextAlign.center,),
+                  SizedBox(height: height * 0.02,),
+                  Text(LocaleKeys.onboarding_onboarding_2_description.tr(),
+                  style: AppStyles.darkGrayRegular20,),
+                  SizedBox(height: height * 0.03,),
+                  
+                  CustomButton(body: Text(LocaleKeys.onboarding_next_button.tr(),style: AppStyles.darkSemiBold20,),
+                  backgroundColor: null ,color: AppColors.primaryYellowColor,onPressed: (){
+                    introKey.currentState?.next();
+                  }),
+
+              ],
+            ),
+          )
+        ),
+         
+
+         PageViewModel(
+          titleWidget: Container(
+         
+          ),
+          backgroundImage: AppAssets.onBoarding3,
+          bodyWidget: Container(
+              margin: EdgeInsets.only(top: height * 0.09),
+            child: Column(
+              children: [
+                 Text(LocaleKeys.onboarding_onboarding_3_title.tr(),style: AppStyles.lightMedium36,
+                  textAlign: TextAlign.center,),
+                  // SizedBox(height: height * 0.02,),
+                  Text(LocaleKeys.onboarding_onboarding_3_description.tr(),
+                  style: AppStyles.darkGrayRegular20,),
+                  SizedBox(height: height * 0.03,),
+                  
+                  CustomButton(body: Text(LocaleKeys.onboarding_next_button.tr(),style: AppStyles.darkSemiBold20,),
+                  backgroundColor: null ,color: AppColors.primaryYellowColor,onPressed: (){
+                    introKey.currentState?.next();
+                  }),
+                  SizedBox(height: height * 0.02,),
+                  CustomButton(body: Text(LocaleKeys.onboarding_back_button.tr(),style: AppStyles.primaryBold20,),
+                  backgroundColor: AppColors.darkGray ,color: null,onPressed: (){
+                    introKey.currentState?.previous();
+                  },borderRadius: 2,),
+
+              ],
+            ),
+          )
+        ),
+
+         PageViewModel(
+          titleWidget: Container(
+          ),
+          backgroundImage: AppAssets.onBoarding4,
+          bodyWidget: Container(
+              margin: EdgeInsets.only(top: height * 0.09),
+            child: Column(
+              children: [
+                 Text(LocaleKeys.onboarding_onboarding_4_title.tr(),style: AppStyles.lightMedium36,
+                  textAlign: TextAlign.center,),
+                  // SizedBox(height: height * 0.02,),
+                  Text(LocaleKeys.onboarding_onboarding_4_description.tr(),
+                  style: AppStyles.darkGrayRegular20,),
+                  SizedBox(height: height * 0.03,),
+                  
+                  CustomButton(body: Text(LocaleKeys.onboarding_next_button.tr(),style: AppStyles.darkSemiBold20,),
+                  backgroundColor: null ,color: AppColors.primaryYellowColor,onPressed: (){
+                    introKey.currentState?.next();
+                  }),
+                  SizedBox(height: height * 0.02,),
+                  CustomButton(body: Text(LocaleKeys.onboarding_back_button.tr(),style: AppStyles.primaryBold20,),
+                  backgroundColor: AppColors.darkGray ,color: null,onPressed: (){
+                    introKey.currentState?.previous();
+                  },borderRadius: 2,),
+
+              ],
+            ),
+          )
+        ),
+
+         PageViewModel(
+          titleWidget: Container(
+            width: double.infinity,
+          ),
+          backgroundImage: AppAssets.onBoarding5,
+          bodyWidget: Container(
+              margin: EdgeInsets.only(top: height * 0.09),
+            child: Column(
+              children: [
+                 Text(LocaleKeys.onboarding_onboarding_5_title.tr(),style: AppStyles.lightMedium36,
+                  textAlign: TextAlign.center,),
+                  SizedBox(height: height * 0.02,),
+                  Text(LocaleKeys.onboarding_onboarding_5_description.tr(),
+                  style: AppStyles.darkGrayRegular20,),
+                  SizedBox(height: height * 0.03,),
+                  
+                  CustomButton(body: Text(LocaleKeys.onboarding_next_button.tr(),style: AppStyles.darkSemiBold20,),
+                  backgroundColor: null ,color: AppColors.primaryYellowColor,onPressed: (){
+                    introKey.currentState?.next();
+                  }),
+                  SizedBox(height: height * 0.02,),
+                  CustomButton(body: Text(LocaleKeys.onboarding_back_button.tr(),style: AppStyles.primaryBold20,),
+                  backgroundColor: AppColors.darkGray ,color: null,onPressed: (){
+                    introKey.currentState?.previous();
+                  },borderRadius: 2,),
+
+              ],
+            ),
+          )
+        ),
+
+         PageViewModel(
+          titleWidget: Container(
+            width: double.infinity,
+          ),
+          backgroundImage: AppAssets.onBoarding6,
+          bodyWidget: Container(
+              margin: EdgeInsets.only(top: height * 0.15),
+            child: Column(
+              children: [
+                 Text(LocaleKeys.onboarding_onboarding_6_button.tr(),style: AppStyles.lightMedium36,
+                  textAlign: TextAlign.center,),
+                 
+                
+                  SizedBox(height: height * 0.03,),
+                  
+                  CustomButton(body: Text(LocaleKeys.onboarding_finish_button.tr(),style: AppStyles.darkSemiBold20,),
+                  backgroundColor: null ,color: AppColors.primaryYellowColor,onPressed: (){
+                    Navigator.pushReplacementNamed(context, RouteNames.login);
+                  }),
+                  SizedBox(height: height * 0.02,),
+                  CustomButton(body: Text(LocaleKeys.onboarding_back_button.tr(),style: AppStyles.primaryBold20,),
+                  backgroundColor: AppColors.darkGray ,color: null,onPressed: (){
+                    introKey.currentState?.previous();
+                  },borderRadius: 2,),
+
+              ],
+            ),
+          )
+        ),
+        
+      ],
+     
+       
+       
+       
+       
+      showNextButton: false,
+      showDoneButton: false,
+      showBackButton: false,
+     );
   }
 }
