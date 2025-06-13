16ee05e8e26dffccf8cc9aeb7fbc4fd5b268dd56
khalid <khalidabdelrazk4@gmail.com>
2025-06-14 00:11:25 +0300

    di.config.dart edited

--- lib/presentation/profile/Data/Repository/profile_repository_impl.dart
+++ lib/presentation/profile/Data/Repository/profile_repository_impl.dart
@@ -1,15 +1,16 @@
-import 'package:either_dart/src/either.dart';
 import 'package:injectable/injectable.dart';
-import 'package:movies/presentation/authentication/Domain/Entity/failures.dart';
-import 'package:movies/presentation/profile/Data/Data%20Sources/remote/get_profile_remote_data_source.dart';
-import 'package:movies/presentation/profile/Domain/Repository/profile_repository.dart';
+import 'package:movies/presentation/profile/Domain/repository/profile_repository.dart';
 import 'package:movies/presentation/profile/Domain/entitys/GetProfileResponseEntity.dart';
+import 'package:movies/presentation/profile/Data/Data Sources/remote/get_profile_remote_data_source.dart';
+import 'package:either_dart/either.dart';
+import 'package:movies/presentation/authentication/Domain/Entity/failures.dart';
 
-@Injectable(as: ProfileRepository) 
-class ProfileRepositoryImpl implements ProfileRepository {
-  GetProfileRemoteDataSource getProfileRemoteDataSource;
+@Injectable(as: ProfileRepository)
+class ProfileRepositoryImpl extends ProfileRepository {
+  final GetProfileRemoteDataSource getProfileRemoteDataSource;
 
   ProfileRepositoryImpl({required this.getProfileRemoteDataSource});
+
   @override
   Future<Either<Failures, GetProfileResponseEntity>> getProfile() async {
     var either = await getProfileRemoteDataSource.getProfile();
