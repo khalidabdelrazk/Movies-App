7c54dfcfd10b6dc3928dbbe516a5842a542ab0cb
rajeh1032 <98366239+rajeh1032@users.noreply.github.com>
2025-06-13 17:52:36 +0300

    feat: Update ProfileRepository and ProfileGetUseCase to remove token parameter and streamline profile retrieval

--- lib/presentation/profile/Domain/repository/profile_repository.dart
+++ lib/presentation/profile/Domain/repository/profile_repository.dart
@@ -1,7 +1,7 @@
 import 'package:either_dart/either.dart';
 import 'package:movies/presentation/authentication/Domain/Entity/failures.dart';
-import 'package:movies/presentation/authentication/Domain/Entity/register_response_entity.dart';
+import 'package:movies/presentation/profile/Domain/entitys/GetProfileResponseEntity.dart';
 
 abstract class ProfileRepository {
-  Future<Either<Failures, RegisterResponseEntity>> getProfile(String? token);
+  Future<Either<Failures, GetProfileResponseEntity>> getProfile();
 }
--- lib/presentation/profile/Domain/use_cases/profile_get_use_case.dart
+++ lib/presentation/profile/Domain/use_cases/profile_get_use_case.dart
@@ -1,13 +1,13 @@
-// import 'package:either_dart/either.dart';
-// import 'package:movies/presentation/authentication/Domain/Entity/failures.dart';
-// import 'package:movies/presentation/profile/Domain/entitys/get_profile_response_entity.dart';
-// import 'package:movies/presentation/profile/Domain/repository/profile_repository.dart';
+import 'package:either_dart/either.dart';
+import 'package:movies/presentation/authentication/Domain/Entity/failures.dart';
+import 'package:movies/presentation/profile/Domain/entitys/GetProfileResponseEntity.dart';
+import 'package:movies/presentation/profile/Domain/repository/profile_repository.dart';
 
-// class ProfileGetUseCase {
-//   ProfileRepository profileRepository;
-//   ProfileGetUseCase({required this.profileRepository});
+class ProfileGetUseCase {
+  ProfileRepository profileRepository;
+  ProfileGetUseCase({required this.profileRepository});
 
-//   Future<Either<Failures, GetProfileResponseEntity>> invoke(String? token) {
-//     return profileRepository.getProfile(token);
-//   }
-// }
+  Future<Either<Failures, GetProfileResponseEntity>> invoke() {
+    return profileRepository.getProfile();
+  }
+}
